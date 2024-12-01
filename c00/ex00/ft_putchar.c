31753705fa635affde5e9e4514ac98b22110932b
Gagao <vinimelosousa@gmail.com>
2024-12-01 22:19:30 +0000

    ate o ex04

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
-{   
-    write (1, &c, 1);
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
 }
 
 /* int main()
 {
-    char teste;
+	char teste;
 
-    teste = 'f';
-    ft_putchar(teste);
+	teste = 'f';
+	ft_putchar(teste);
 }
 */
